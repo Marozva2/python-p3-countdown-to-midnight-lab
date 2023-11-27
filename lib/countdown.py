7718424c28e4e8ba1f774d361a2ab6be299d30d3
Marozva <isabokefrankline@gmail.com>
2023-11-27 17:42:01 +0300

    using a while loop instead

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,13 +1,16 @@
 import time
 
 def countdown(seconds):
-    for i in range(seconds, 0, -1):
+    i = seconds
+    while i > 0:
         print(f"{i} SECOND(S)!")
+        i -= 1
     print("HAPPY NEW YEAR!")
-    
 
 def countdown_with_sleep(seconds):
-    for i in range(seconds, 0, -1):
+    i = seconds
+    while i > 0:
         print(f"{i} SECOND(S)!")
         time.sleep(1)
+        i -= 1
     print("HAPPY NEW YEAR!")
